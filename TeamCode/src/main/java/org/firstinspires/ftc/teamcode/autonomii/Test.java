059e8ea1ad318b639ade3ae3228a33c1acebc804
stlgolfer <amigala@micds.org>
2019-10-15 19:35:19 -0500

    Started Setting Up Vuforia Instances

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomii/Test.java
@@ -0,0 +1,82 @@
+package org.firstinspires.ftc.teamcode.autonomii;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
+
+import java.io.File;
+
+public class Test extends LinearOpMode {
+
+    OpenGLMatrix lastLocation = null;
+
+    int captureCounter = 0;
+    File captureDirectory = AppUtil.ROBOT_DATA_DIR;
+
+    // vuforia instance
+    VuforiaLocalizer vuforia;
+
+    WebcamName webcamName;
+
+    @Override
+    public void runOpMode() {
+        // init
+        webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        // vuforia paramter settings
+        parameters.vuforiaLicenseKey = "AZDepIf/////AAAAGfXxylZkt0YriAZz29imD+JnpWB4sxwIldmqfmE2S0NQ5QJ+R8FF9kqvBAeUoFLVcXawrLuNS1salfES/URf32WEkCus6PRLYzToyuvGnoBHtXJBW9nr94CSnAFvWjPrYVMEQhy7kZeuMEkhvUn8O/4DZ7f8vP1hPC7xKugpmGY0LTvxd/umhQxy9dl28mkUQWHcselYnHrOgrW4XvNq5exF67YoK3cQDjrodu02wmmFcoeHr78xyabZqOif8hk9Lk+F/idAMZcB1un86Goawbto6qTP7/SnXAbAedRrSKCGp/UuYa02c2Y5rteZMMtdSE7iL824A4kmwVZtg5biQy3jE0zAjsFQD7tztRiMGLxt";
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+        vuforia.enableConvertFrameToBitmap();
+        AppUtil.getInstance().ensureDirectoryExists(captureDirectory);
+
+        // load trackables
+        VuforiaTrackables skystone = this.vuforia.loadTrackablesFromAsset("SkyStone2");
+
+        // load all trackables
+
+
+
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            for (VuforiaTrackable trackable : allTrackables) {
+                /**
+                 * getUpdatedRobotLocation() will return null if no new information is available since
+                 * the last time that call was made, or if the trackable is not currently visible.
+                 * getRobotLocation() will return null if the trackable is not currently visible.
+                 */
+                telemetry.addData(trackable.getName(), ((VuforiaTrackableDefaultListener)trackable.getListener()).isVisible() ? "Visible" : "Not Visible");    //
+
+                OpenGLMatrix robotLocationTransform = ((VuforiaTrackableDefaultListener)trackable.getListener()).getUpdatedRobotLocation();
+                if (robotLocationTransform != null) {
+                    lastLocation = robotLocationTransform;
+                }
+            }
+            /**
+             * Provide feedback as to where the robot was last located (if we know).
+             */
+            if (lastLocation != null) {
+                //  RobotLog.vv(TAG, "robot=%s", format(lastLocation));
+                telemetry.addData("Pos", format(lastLocation));
+            } else {
+                telemetry.addData("Pos", "Unknown");
+            }
+            telemetry.update();
+        }
+
+        // game start
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/SkystoneTargetController.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.lib;
+
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+public class SkystoneTargetController {
+    private VuforiaTrackables trackables;
+
+    // all trackables
+    private VuforiaTrackable RedPerimiterTarget2;
+    private VuforiaTrackable RedPerimiterTarget1;
+    private VuforiaTrackable RearPerimeterTarget2;
+    private VuforiaTrackable RearPerimeterTarget1;
+    private VuforiaTrackable FrontPerimeterTarget2;
+    private VuforiaTrackable FrontPerimieterTarget1;
+    private VuforiaTrackable BluePerimeterTarget2;
+    private VuforiaTrackable BluePerimeterTarget1;
+}
