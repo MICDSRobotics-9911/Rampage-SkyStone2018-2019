a4c2c470db33b59d9630414f58d159df0710f263
stlgolfer <amigala@micds.org>
2019-11-18 19:51:24 -0600

    Got basic autonomous structure, some of autonomous works
    Need to check on the starting position to see if it's legal

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomii/DetectSkyStone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomii/DetectSkyStone.java
@@ -4,17 +4,29 @@ import android.graphics.Color;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
+import org.firstinspires.ftc.teamcode.lib.AutonomousConstants;
+import org.firstinspires.ftc.teamcode.robotplus.autonomous.TimeOffsetVoltage;
 import org.firstinspires.ftc.teamcode.robotplus.hardware.MecanumDrive;
 import org.firstinspires.ftc.teamcode.robotplus.hardware.Robot;
 
 @Autonomous(name = "DetectSkyStone", group = "Test")
-public class DetectSkyStone extends LinearOpMode {
+public class DetectSkyStone extends LinearOpMode implements AutonomousConstants {
 
     private Robot robot;
     private MecanumDrive mecanumDrive;
     private ColorSensor colorSensor;
+    private DcMotor arm;
+    private DcMotor elevator;
+    private Servo grabber;
+    private Servo clamp;
+    private TouchSensor touchSensorLeft;
+    private TouchSensor touchSensorRight;
 
 
     private float hsvValues[] = {0F, 0F, 0F};
@@ -26,17 +38,24 @@ public class DetectSkyStone extends LinearOpMode {
         this.robot = new Robot(hardwareMap);
         this.mecanumDrive = (MecanumDrive) this.robot.getDrivetrain();
         this.colorSensor = hardwareMap.get(ColorSensor.class, "c1");
+        this.arm = hardwareMap.get(DcMotor.class, "arm");
+        this.elevator = hardwareMap.get(DcMotor.class, "elevator");
+        this.grabber = hardwareMap.get(Servo.class, "grabber");
+        this.clamp = hardwareMap.get(Servo.class, "clamp");
+        this.touchSensorLeft = hardwareMap.get(TouchSensor.class, "left_touch");
+        this.touchSensorRight = hardwareMap.get(TouchSensor.class, "right_touch");
 
         waitForStart();
 
 
         while (opModeIsActive()) {
             // update colors
-            Color.RGBToHSV((int)(colorSensor.red() * this.SCALE_FACTOR),
-                    (int)(colorSensor.green() * this.SCALE_FACTOR),
-                    (int)(colorSensor.blue() * this.SCALE_FACTOR),
+            Color.RGBToHSV((int) (colorSensor.red() * this.SCALE_FACTOR),
+                    (int) (colorSensor.green() * this.SCALE_FACTOR),
+                    (int) (colorSensor.blue() * this.SCALE_FACTOR),
                     this.hsvValues
             );
+
 
             telemetry.addData("Step", this.step);
             telemetry.addData("Red", this.hsvValues[0]);
@@ -45,13 +64,73 @@ public class DetectSkyStone extends LinearOpMode {
             telemetry.addData("Alpha", this.colorSensor.alpha());
             telemetry.update();
 
-            if ((((int)this.hsvValues[0]) < 85)) {
-                this.mecanumDrive.complexDrive(0, 1, 0);
-            }
-            else {
-                // found a skystone
-                //this.step++;
-                this.mecanumDrive.stopMoving();
+            switch (step) {
+                case 0:
+                    // TODO: 11/18/2019 check to see if we've hit the depot wall using another color sensor
+                    if ((((int) this.hsvValues[0]) < 85)) {
+                        this.mecanumDrive.complexDrive(MecanumDrive.Direction.RIGHT.angle(), 1, 0);
+                    } else {
+                        // found a skystone
+                        //this.step++;
+                        this.mecanumDrive.stopMoving();
+                        step++;
+                    }
+                    break;
+                case 1:
+                    // move backwards
+                    this.mecanumDrive.complexDrive(MecanumDrive.Direction.DOWN.angle(), 1, 0);
+                    // TODO: 11/18/2019 implement voltage sensor
+                    sleep(150);
+                    this.mecanumDrive.stopMoving();
+                    step++;
+                    break;
+                case 2:
+                    // drop the arm
+                    this.arm.setPower(-0.5);
+                    sleep(AutonomousConstants.ARM_DROP_DISTANCE);
+                    this.arm.setPower(0);
+                    step++;
+                    break;
+                case 3:
+                    // open the claw and grab the skystone
+                    this.grabber.setPosition(0.25);
+                    sleep(1500);
+                    this.grabber.setPosition(0);
+                    step++;
+                    break;
+                case 4:
+                    // move arm back up a bit
+                    this.arm.setPower(1);
+                    sleep(550);
+                    this.arm.setPower(0);
+                    step++;
+                    break;
+                case 5:
+                    // move towards the foundation by hitting a wall
+                    if (!this.touchSensorLeft.isPressed()) {
+                        this.mecanumDrive.complexDrive(MecanumDrive.Direction.LEFT.angle(), 1, 0);
+                    }
+                    else {
+                        this.mecanumDrive.stopMoving();
+                    }
+                    break;
+                case 6:
+                    // down the clamp
+                    this.clamp.setPosition(0);
+                    break;
+                case 7:
+                    // drag the foundation until a button press (it's against a wall)
+                    if (!this.touchSensorLeft.isPressed()) {
+                        this.mecanumDrive.complexDrive(0, 1, 0);
+                    }
+                    else {
+                        this.mecanumDrive.stopMoving();
+                    }
+                    break;
+                case 8:
+                    // open the clamp
+                    this.clamp.setPosition(1);
+                    break;
             }
         }
     }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/AutonomousConstants.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.lib;
+
+/**
+ * Constants for autonomous
+ * @since 11/18/19
+ */
+public interface AutonomousConstants {
+
+    /**
+     * Sleep time (in miliseconds) for the arm to swing over the back of the robot
+     */
+    int ARM_DROP_DISTANCE = 1500;
+}
